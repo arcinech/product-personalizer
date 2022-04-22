1755b1f08b25cdee822d2df59fef8745c02178fe
Marcin Lech <arcinech@gmail.com>
2022-04-22 21:30:53 +0200

    Fix: key error

--- src/components/OptionSize/OptionSize.js
+++ src/components/OptionSize/OptionSize.js
@@ -1,15 +1,14 @@
 import styles from './OptionSize.module.scss';
 import clsx from 'clsx';
-
 const OptionSize = props => {
 
   return (
     <div className={styles.sizes}>
     <h3 className={styles.optionLabel}>Sizes</h3>
     <ul className={styles.choices}>
-      {props.sizes.map((size) => { 
+      {props.sizes.map((size, i) => { 
         return (
-          <li key={size}>
+          <li key={i}>
             <button 
               onClick={() => props.setCurrentSize(size)}
               type="button" 
